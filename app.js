4265874e4beb89d7939db16e930a4a73d180b217
notFraps <123653131+notFraps@users.noreply.github.com>
2024-03-21 03:40:50 +0100

    Add gitignore, Procfile

--- app.js
+++ app.js
@@ -118,6 +118,11 @@ app.post("/", function (req, res) {
     
 });
 
-app.listen(3000, function (req, res) {
-    console.log("The app is running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function (req, res) {
+    console.log("The app is running on server successfully");
 })
