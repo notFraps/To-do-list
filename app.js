84422fb44a0db60e87be8552b54da94bcee8fcd6
notFraps <123653131+notFraps@users.noreply.github.com>
2024-04-26 07:25:02 +0100

    Listen up

--- app.js
+++ app.js
@@ -118,4 +118,8 @@ app.post("/", function (req, res) {
     
 });
 
-app.listen(process.env.PORT);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
